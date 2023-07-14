b265c44db178b516db2f693dc10b9454a27ba3ac
planetarybeing <awatkis00@icloud.com>
2023-07-13 20:08:30 -0400

    Uploaded 1st draft of resume

--- components/home/contact/Contact.tsx
+++ components/home/contact/Contact.tsx
@@ -16,7 +16,7 @@ export const Contact = () => {
           <p className={styles.contactCopy}>
             Shoot me an email if you want to connect! You can also find me on{" "}
             <Link
-              href="https://www.linkedin.com/in/anthony-watkis-424707225"
+              href="https://www.linkedin.com/in/anthony-a-watkis"
               target="_blank"
               rel="nofollow"
             >
